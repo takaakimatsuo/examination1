5dc95e96e019a0ebd3d8a141e38ffdf282c3fa4a
matsuot34 <takaaki.matsuo02@g.softbank.co.jp>
2020-02-28 20:48:07 +0900

    TODO1.c: entryエンドポイントのコントローラの実装

--- src/main/java/jp/co/softbank/cxr/exam/application/controller/RecipeController.java
+++ src/main/java/jp/co/softbank/cxr/exam/application/controller/RecipeController.java
@@ -1,16 +1,27 @@
 package jp.co.softbank.cxr.exam.application.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * レシピ管理システムのコントローラーを表すクラスです.
+ * レシピ管理システムの REST コントローラー.
  */
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class RecipeController {
+  /**
+   * entry のエンドポイント.
+   */
+  @GetMapping(path = "/")
+  @ResponseStatus(HttpStatus.OK)
+  public void entry() {
+    log.debug("Request sent to entry endpoint.");
+  }
+
 
 }
